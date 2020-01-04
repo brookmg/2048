63a4fdabbb91cd447910f4ebefe046fb8c852815
brookmezgu <brookmezgebu64@gmail.com>
2020-01-04 23:15:48 +0300

    progress: add new activity to serve as the container

---
+++ 2048/2048/src/main/java/app/g2048/android/ui/activity/CoreActivity.java
@@ -0,0 +1,18 @@
+package app.g2048.android.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.g2048.android.R;
+
+public class CoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core);
+
+
+    }
+}
